5085148aaf678d8a70f89a4168e4c1a28e35f6dd
jose febres <jfebres90@gmail.com>
2014-12-14 00:03:41 -0500

    active side nav

--- js/main.js
+++ js/main.js
@@ -8,6 +8,65 @@ $(document).ready(function(){
   });
 
 
+        $('.one').hide();
+        $('.two').hide();
+        $('.three').hide();
+        $('.four').hide();
+
+    $('li').on('click',function(){
+
+
+    	$(this).addClass("highlight")
+
+     if( $(this).is(".link-one")){
+     	
+     	$('.three').hide()
+     	$('.two').hide();
+		$('.one').show();
+		//$( '.link-one' ).addClass( "high" );
+		$( '.link-two' ).removeClass( "highlight" );
+     	$( '.link-three' ).removeClass( "highlight" );
+     }
+     else if( $(this).is(".link-two")){
+     	
+     		$('.one').hide();
+     		$('.three').hide()
+     		$('.two').show();
+     		//$( '.link-two').addClass( "high" );
+     		$( '.link-one' ).removeClass( "highlight" );
+     		$( '.link-three' ).removeClass( "highlight" );
+
+     }
+      else if( $(this).is(".link-three")){
+     	
+     		$('.one').hide();
+     		$('.two').hide();
+     		$('.three').show()
+     		//$( '.link-three').addClass( "high" );
+     		$( '.link-one' ).removeClass( "highlight" );
+     		$( '.link-two' ).removeClass( "highlight" );
+     }
+
+  });
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
