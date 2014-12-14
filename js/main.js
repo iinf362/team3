c679a956ba7193134a90c478505c26db241a22dd
jose febres <jfebres90@gmail.com>
2014-12-14 00:20:18 -0500

    update js

--- js/main.js
+++ js/main.js
@@ -1,68 +1,53 @@
 $(document).ready(function(){
 
-        $('.info').hide();
+	$('.info').hide();
 
-  $('button').on('click',function(){
-    
-     $(this).next('div').slideToggle(1000);
-  });
+	$('button').on('click',function(){
 
-
-        $('.one').hide();
-        $('.two').hide();
-        $('.three').hide();
-        $('.four').hide();
-
-    $('li').on('click',function(){
+		$(this).next('div').slideToggle(1000);
+	});
 
 
-    	$(this).addClass("highlight")
+	$('.one').hide();
+	$('.two').hide();
+	$('.three').hide();
+	$('.four').hide();
 
-     if( $(this).is(".link-one")){
-     	
-     	$('.three').hide()
-     	$('.two').hide();
-		$('.one').show();
+	$('.sidebar ul li').on('click',function(){
+
+
+		$(this).addClass("highlight")
+
+		if( $(this).is(".link-one")){
+
+			$('.three').hide()
+			$('.two').hide();
+			$('.one').show();
 		//$( '.link-one' ).addClass( "high" );
 		$( '.link-two' ).removeClass( "highlight" );
-     	$( '.link-three' ).removeClass( "highlight" );
-     }
-     else if( $(this).is(".link-two")){
-     	
-     		$('.one').hide();
-     		$('.three').hide()
-     		$('.two').show();
+		$( '.link-three' ).removeClass( "highlight" );
+	}
+	else if( $(this).is(".link-two")){
+
+		$('.one').hide();
+		$('.three').hide()
+		$('.two').show();
      		//$( '.link-two').addClass( "high" );
      		$( '.link-one' ).removeClass( "highlight" );
      		$( '.link-three' ).removeClass( "highlight" );
 
-     }
-      else if( $(this).is(".link-three")){
-     	
+     	}
+     	else if( $(this).is(".link-three")){
+
      		$('.one').hide();
      		$('.two').hide();
      		$('.three').show()
      		//$( '.link-three').addClass( "high" );
      		$( '.link-one' ).removeClass( "highlight" );
      		$( '.link-two' ).removeClass( "highlight" );
-     }
+     	}
 
-  });
-
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
+     });
 
 
 
@@ -71,5 +56,5 @@ $(document).ready(function(){
 
 
 function scrollWin() {
-    window.scrollTo(500, 0);
+	window.scrollTo(500, 0);
 }
